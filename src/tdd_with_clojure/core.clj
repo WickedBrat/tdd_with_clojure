ef939b345d16107fc2ec4e87df50c7954aa64af4
WickedBrat <harshsrivastav123@gmail.com>
2020-07-21 22:10:01 +0530

    seggregated to shape.clj

--- src/tdd_with_clojure/core.clj
+++ src/tdd_with_clojure/core.clj
@@ -1,27 +1,9 @@
 (ns tdd-with-clojure.core
-  (:require [clojure.string :as strings])
+  (:require [tdd-with-clojure.shape :as print_shapes])
   (:gen-class))
-
-(defn print_square
-  [side]
-  (def square (vec (repeat side (vec (repeat side " ")))))
-  
-  (doseq [row (range 0 (inc (- side 1)))
-          col (range 0 (inc (- side 1)))]
-    (if (or (= row 0)
-            (= row (- side 1))
-            (= col 0)
-            (= col (- side 1)))
-      (def square (assoc square row (assoc (get square row) col "#")))
-    )
-  )
-  (doseq [row (range 0 (inc (- side 1)))]
-    (println (strings/join "" (get square row)))
-  )
-)
 
 
 (defn -main
   [& args]
-  (print_square 5)
+  (print_shapes/square 5)
 )
---
+++ src/tdd_with_clojure/shape.clj
@@ -0,0 +1,21 @@
+(ns tdd-with-clojure.shape
+  (:require [clojure.string :as strings])
+  (:gen-class))
+
+(defn square
+  [side]
+  (def square (vec (repeat side (vec (repeat side " ")))))
+  
+  (doseq [row (range 0 (inc (- side 1)))
+          col (range 0 (inc (- side 1)))]
+    (if (or (= row 0)
+            (= row (- side 1))
+            (= col 0)
+            (= col (- side 1)))
+      (def square (assoc square row (assoc (get square row) col "#")))
+    )
+  )
+  (doseq [row (range 0 (inc (- side 1)))]
+    (println (strings/join "" (get square row)))
+  )
+)
