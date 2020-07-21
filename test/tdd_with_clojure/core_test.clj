078223150f938a698ffad5c42626fd750b42aae9
WickedBrat <harshsrivastav123@gmail.com>
2020-07-22 01:08:30 +0530

    Adds tests for diamond

--- test/tdd_with_clojure/core_test.clj
+++ test/tdd_with_clojure/core_test.clj
@@ -5,8 +5,16 @@
 (deftest test-square
   (testing "Matches correct generation of vectors for square of 5 units"
     (def square (vec (tdd-with-clojure.shape/generate_square 5)))
-    (is (= (get square 0) ["#" "#" "#" "#" "#"] ))
+    (is (= (get square 0) ["#" "#" "#" "#" "#"]))
     (is (= (get square 1) ["#" " " " " " " "#"]))
     (is (= (get square 2) ["#" " " " " " " "#"]))
     (is (= (get square 3) ["#" " " " " " " "#"]))
     (is (= (get square 4) ["#" "#" "#" "#" "#"]))))
+
+
+(deftest test-diamond
+  (testing "Matches correct generation of vectors for diamond of 3 units"
+    (def diamond (vec (tdd-with-clojure.shape/generate_diamond 3)))
+    (is (= (get diamond 0) [" " "#" " "]))
+    (is (= (get diamond 1) ["#" " " "#"]))
+    (is (= (get diamond 2) [" " "#" " "]))))
