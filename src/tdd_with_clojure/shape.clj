2722977998fd5be1a04fde3cf5216a32e663ab76
giridharavinash <giridharavinash93@gmail.com>
2020-07-22 16:27:06 +0530

    Updated render function

--- src/tdd_with_clojure/shape.clj
+++ src/tdd_with_clojure/shape.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as strings])
   (:gen-class))
 
-(defn generate_square
+(defn square
   [side]
   (def square_array (vec (repeat side (vec (repeat side " ")))))
   
@@ -17,8 +17,9 @@
     )
   square_array
   )
-(defn generate_diamond
-		[width]
+(defn diamond
+		[height]
+		(def width (+ (* 2 (quot height 2)) 1))
 		(def diamond_array (vec (repeat width (vec (repeat width " ")))))
   (def mid (quot width 2))
   (doseq [row (range 0 (inc mid))
@@ -35,25 +36,16 @@
   				  (def diamond_array (assoc diamond_array row (assoc (get diamond_array row) col "#")))
   				)
   )
+
   diamond_array
 )
 
+
+
+
 (defn render
-  [user_input]
-  
-  (def function_name (get (strings/split user_input #" ") 0))
-  (def side_width (Integer.(get (strings/split user_input #" ") 1)))
-  (if (= function_name "square")
-  ((def square_array (vec (generate_square side_width)))
-    (doseq [row (range 0 (inc (- side_width 1)))]
-      (println (strings/join "" (get square_array row))))
-  ))
-
-  (if (= function_name "diamond")
-  ((def diamond_array (vec (generate_diamond side_width)))
-    (doseq [row (range 0 (inc (- side_width 1)))]
-      (println (strings/join "" (get diamond_array row))))
-  ))
-
-
+			[final_array]
+			(doseq [row (range 0 (inc (- (count final_array) 1)))]
+      (println (strings/join "" (get final_array row))))
 )
+
