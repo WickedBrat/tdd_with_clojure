c3aa68b2cddf30f69961e541a349e40ba902047d
WickedBrat <harshsrivastav123@gmail.com>
2020-07-27 23:33:39 +0530

    modified combine function

--- src/tdd_with_clojure/shape.clj
+++ src/tdd_with_clojure/shape.clj
@@ -5,23 +5,23 @@
 (defn square
   ([side] (square 0 0 (- side 1) []))
   ([x-cord y-cord side square_array]
-    (let [x (concat [] (if (<= x-cord side)
-                         (if (= y-cord side)
-                           ["#"]
-                           (let [temp_array
-                                 (if (or (= x-cord 0)
-                                         (= x-cord side)
-                                         (= y-cord 0))
-                                   (concat ["#"] (square x-cord (inc y-cord) side square_array))
-                                   (concat [" "] (square x-cord (inc y-cord) side square_array)))]
-                             (if (= (count temp_array) (+ side 1))
-                               (square (inc x-cord) 0 side (concat square_array temp_array))
-                               temp_array)))
-                         square_array))] x)))
+   (let [x (concat [] (if (<= x-cord side)
+                        (if (= y-cord side)
+                          ["#"]
+                          (let [temp_array
+                                (if (or (= x-cord 0)
+                                        (= x-cord side)
+                                        (= y-cord 0))
+                                  (concat ["#"] (square x-cord (inc y-cord) side square_array))
+                                  (concat [" "] (square x-cord (inc y-cord) side square_array)))]
+                            (if (= (count temp_array) (+ side 1))
+                              (square (inc x-cord) 0 side (concat square_array temp_array))
+                              temp_array)))
+                        square_array))] x)))
 
 
 (defn diamond
-		[width]
+  [width]
   (let [mid (quot width 2)]
     (if (= (rem width 2) 1)
       (loop [x-cord 0]
@@ -72,49 +72,43 @@
                 (print " "))
               (recur (inc y-cord))))
           (println "")
-          (recur (inc x-cord))))))
-  )
+          (recur (inc x-cord)))))))
 
 
 (defn triangle
   ([side] (triangle 0 0 (- side 1) []))
   ([x-cord y-cord side triangle_array]
-  (let [x (concat [] (if (<= x-cord side)
-                       (if (= y-cord side)
-                         (if (= x-cord side)
-                           ["#"]
-                           [" "])
-                         (let [temp_array
-                               (if (or (= x-cord y-cord)
-                                       (= x-cord side)
-                                       (= y-cord 0))
-                                 (concat ["#"] (triangle x-cord (inc y-cord) side triangle_array))
-                                 (concat [" "] (triangle x-cord (inc y-cord) side triangle_array)))]
-                           (if (= (count temp_array) (+ side 1))
-                             (triangle (inc x-cord) 0 side (concat triangle_array temp_array))
-                             temp_array)))
-                       triangle_array))] x))
-)
+   (let [x (concat [] (if (<= x-cord side)
+                        (if (= y-cord side)
+                          (if (= x-cord side)
+                            ["#"]
+                            [" "])
+                          (let [temp_array
+                                (if (or (= x-cord y-cord)
+                                        (= x-cord side)
+                                        (= y-cord 0))
+                                  (concat ["#"] (triangle x-cord (inc y-cord) side triangle_array))
+                                  (concat [" "] (triangle x-cord (inc y-cord) side triangle_array)))]
+                            (if (= (count temp_array) (+ side 1))
+                              (triangle (inc x-cord) 0 side (concat triangle_array temp_array))
+                              temp_array)))
+                        triangle_array))] x)))
 
 (defn combine
-		[& args]
-		(def max_size 0)
-		(doseq [shape (range (count args))]
-					(def max_size (max max_size (count (nth args shape)))))
-		(def final_array (vec (repeat max_size (vec (repeat max_size " ")))))
+  [& args]
 
-		(doseq [each (range (count args))]
-				(def array  (nth args each))
-				(doseq [row (range (count array))
-											col (range (count array))]
-						(if (or (= (get (get final_array row) col) "#")
-														(= (get (get array row) col) "#")
-											)
-											(def final_array (assoc final_array row (assoc (get final_array row) col "#")))
-											(def final_array (assoc final_array row (assoc (get final_array row) col " ")))  
-							)))
-		 final_array
-)
+  (let [max-size (apply max (let [lengths (map count args)] lengths))]
+    ((def final_array (vec (repeat max-size " ")))
+     (loop [array_index 0]
+       (when (< array_index (count args))
+         (loop [index 0]
+           (when (< index (count (nth args array_index)))
+             (if (= (nth (nth args array_index) index) "#")
+               (assoc final_array index "#"))
+             (recur (inc index))))
+         (recur (inc array_index))))
+     )
+    final_array))
 
 (defn print-array
   [array, width]
